f28ba219bbbcc9671a5a24517619d1f7485f7643
Arkashish Sutradhar <arkashishdhar1@gmail.com>
2023-06-05 17:58:05 +0530

    Completed consumption and sending mail

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
 const express = require('express');
-
+const amqplib = require('amqplib');
+const { EmailService } = require('./services')
+async function connectQueue() {
+    try {
+        // const conn = await amqlib.connect("amqp://localhost");
+        // const channel = await conn.createChannel();
+        // await channel.assertQueue('noti-queue');
+        // channel.consume('noti-queue', async (data) => {
+        //     // console.log(`${Buffer.from(data.content)}`);
+        //     const object = JSON.parse(Buffer.from(data).toString());
+        //     await EmailService.sendEmail("arkashish.sutradhar.cse23@heritageit.edu.in", object.recepientEmail, object.subject, object.text);
+        //     channel.ack(data);
+        // })
+        const connection = await amqplib.connect("amqp://localhost");
+        const channel = await connection.createChannel();
+        await channel.assertQueue("noti-queue");
+        channel.consume("noti-queue", async (data) => {
+            console.log(`${Buffer.from(data.content)}`);
+            const object = JSON.parse(`${Buffer.from(data.content)}`);
+            // const object = JSON.parse(Buffer.from(data).toString());
+            await EmailService.sendEmail("arkashish.sutradhar.cse23@heritageit.edu.in", object.recepientEmail, object.subject, object.text);
+            channel.ack(data);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
 
@@ -14,4 +40,6 @@ app.use('/api', apiRoutes);
 
 app.listen(ServerConfig.PORT, async () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+    await connectQueue();
+    console.log('queue is up');
 });
