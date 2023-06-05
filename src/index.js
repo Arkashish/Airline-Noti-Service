7c8e2fa52af18213f0a517d12f7eb6743e99a01f
Arkashish Sutradhar <arkashishdhar1@gmail.com>
2023-06-05 14:27:49 +0530

    Added create ticket api

--- src/index.js
+++ src/index.js
@@ -14,17 +14,4 @@ app.use('/api', apiRoutes);
 
 app.listen(ServerConfig.PORT, async () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
-    try {
-        const res = await mailsender.sendMail({
-            from: ServerConfig.GMAIL_EMAIL,
-            to: 'ashismunmun11@gmail.com',
-            subject: 'Is this service working',
-            text: 'Yes its working'
-        });
-        console.log(res);
-    } catch (error) {
-        console.log(error);
-    }
-
-
 });
